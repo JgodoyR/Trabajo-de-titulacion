6f542f356209f5d2955b48cde84a2d3ca438cf92
JgodoyR <jordan.godoy@usach.cl>
2022-07-30 19:45:12 +0000

    ajustes

--- TrabajoTitulacion/settings.py
+++ TrabajoTitulacion/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'Niko2794*',
         'HOST': 'db',
         #'HOST': '127.0.0.1',
         'DATABASE_PORT': '5432',
--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -3,7 +3,7 @@
     class="mx-auto">
     <v-card-title class="text-center justify-center py-6">
       <h1 class="font-weight-bold text-h5 white--text">
-        Progreso de {{ items.nombres }} {{ items.apellido_paterno }} {{ items.apellido_materno }}
+        Progreso de {{ items.nombres }} {{ items.paterno }} {{ items.materno }}
       </h1>
     </v-card-title>
     
